03773912e2c8ba97438aa2e2c3f9cab7656149d5
miracode <rascatim@gmail.com>
2014-11-22 13:35:51 -0800

    refactor home page view to use template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -14,7 +15,7 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+        expected_html = render_to_string('home.html')
         # note: conent is in raw bytes
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        # use decode() to convert bytes into python
+        self.assertEqual(response.content.decode(), expected_html)
