902e0f9af8fad81a71fad2503984744848a67279
miracode <rascatim@gmail.com>
2014-11-22 13:23:21 -0800

    Functional test checks input of to-do, and added Chrome instead of Firefox for browser

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -6,7 +7,7 @@ class NewVisitorTest(unittest.TestCase):
 
     # special methods which run before and after each test
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
@@ -18,22 +19,36 @@ class NewVisitorTest(unittest.TestCase):
 
         # User notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # User is invited to ender a to-do item straight away
+        inputbox = self.browser.find_element_by_tag_name('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a to-do item')
 
         # User types "Buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
 
         # When user hits enter, the page updates and pages lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        # element returns element, raising exception if not found
+        table = self.browser.find_element_by_id('id_list_table')
+        # element*s* returns a list (which may be empty)
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows))
 
         # There is still a text box inviting user to add another item
         # user enters "Use peacock feathers to make a fly"
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
         # User wonders whether the site will remember her list.  She then sees
-        # that the site has generated a unique URL for her -- there is some 
+        # that the site has generated a unique URL for her -- there is some
         # explanitory text to that effect
 
         # She visits that URL - her to-do list is still there.
