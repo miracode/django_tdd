868f7e3699304e2c42fe9d0d597e003fefb0a7c1
miracode <rascatim@gmail.com>
2014-11-23 14:17:32 -0800

    supports one list item

--- functional_tests.py
+++ functional_tests.py
@@ -38,19 +38,27 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         # element*s* returns a list (which may be empty)
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table")
+        self.assertIn('1: Buy peacock feathers',
+                      [row.text for row in rows])
 
         # There is still a text box inviting user to add another item
         # user enters "Use peacock feathers to make a fly"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers',
+                      [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+                      [row.text for row in rows])
 
         # User wonders whether the site will remember her list.  She then sees
         # that the site has generated a unique URL for her -- there is some
         # explanitory text to that effect
+        self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
