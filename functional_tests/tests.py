1df3276180b9b56919f4aa2b54fbe17fc9c392ea
miracode <rascatim@gmail.com>
2014-11-24 13:23:01 -0800

    used bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     # special methods which run before and after each test
     def setUp(self):
