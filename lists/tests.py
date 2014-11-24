33d7acf2a29dd0002d736165d7b91c3aaa972001
miracode <rascatim@gmail.com>
2014-11-23 20:27:47 -0800

    changed post action in templates

--- lists/tests.py
+++ lists/tests.py
@@ -21,11 +21,6 @@ class HomePageTest(TestCase):
         # use decode() to convert bytes into python
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ItemModelTest(TestCase):
 
